e33b7bf638d55e85fe0736a59ca40c6de60b0d14
omour-faruk <omour.faruk@live.com>
2024-07-03 00:44:11 +0600

    Update

---
+++ dataType_variables.py
@@ -0,0 +1,23 @@
+#Task:
+#Create variables to store the following data types: integer, float, string, boolean.
+#Print the types of these variables using the type() function.
+#Perform basic arithmetic operations (addition, subtraction, etc.) with the numerical variables.
+
+number = 27
+decimal= 3.14
+name = "Omour"
+A = True
+
+print(type(number))
+print(type(decimal))
+print(type(name))
+print(type(A)) 
+
+num1 = 330 
+num2 = 10 
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
