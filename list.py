4fa6ce07cb0324ac56b5c48b1d5bbaa29fc53e3d
omour-faruk <omour.faruk@live.com>
2024-07-05 00:09:46 +0600

    Update

---
+++ list.py
@@ -0,0 +1,11 @@
+fav_fruits = ["Mango", "Apple", "Banana", "Strawberry", "Lychee", "Jackfruit"]
+
+fav_fruits.append("Grapes")
+
+fav_fruits.sort()
+
+fruit_length = [len(fruit) for fruit in fav_fruits]
+
+
+print(fav_fruits)
+print(fruit_length)
